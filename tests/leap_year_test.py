36a53ff1d81904b3a911f8dcf6bfc3de6becd127
Leanne <lkeeley@pace.edu>
2023-10-20 12:40:38 -0400

    leap year test, look for [missing\ not|n't

--- tests/leap_year_test.py
+++ tests/leap_year_test.py
@@ -2,34 +2,35 @@ import re
 import leap_year
 
 def test_leap_year_1996(capsys, monkeypatch):
-    inputs, output = ("1996", "leap year")
+    inputs, outputs = ("1996", [r"^((?!not).)*$", r"^((?!n't).)*$"])
     monkeypatch.setattr('builtins.input', lambda _: inputs)
     leap_year.main()
     captured = capsys.readouterr()
-    assert output in captured.out.lower()
+    for output in outputs:
+        assert re.search(output, captured.out.lower(), re.IGNORECASE)
 
 def test_leap_year_2000(capsys, monkeypatch):
-    inputs, output = ("2000", "leap year")
+    # reverse negative look up for word not
+    inputs, outputs = ("2000", [r"^((?!not).)*$", r"^((?!n't).)*$"])
     monkeypatch.setattr('builtins.input', lambda _: inputs)
     leap_year.main()
     captured = capsys.readouterr()
-    assert output in captured.out.lower()
+    for output in outputs:
+        assert re.search(output, captured.out.lower(), re.IGNORECASE)
 
 def test_leap_year_1800(capsys, monkeypatch):
-    inputs, outputs = ("1800", ["not", "a leap year"])
+    inputs, output = ("1800", r"[not|n't]")
     monkeypatch.setattr('builtins.input', lambda _: inputs)
     leap_year.main()
     captured = capsys.readouterr()
-    for output in outputs:
-        assert output in captured.out.lower()
+    assert re.search(output, captured.out.lower(), re.IGNORECASE)
 
 def test_leap_year_1999(capsys, monkeypatch):
-    inputs, outputs = ("1999", ["not", "a leap year"])
+    inputs, output = ("1999", r"[not|n't]")
     monkeypatch.setattr('builtins.input', lambda _: inputs)
     leap_year.main()
     captured = capsys.readouterr()
-    for output in outputs:
-        assert output in captured.out.lower()
+    assert re.search(output, captured.out.lower(), re.IGNORECASE)
 
 def test_leap_year_str(monkeypatch):
     # test that program works with a non valid input
