f8d215bf7b12fa98db916f9c5327eb5398c7b0e0
Leanne <lkeeley@pace.edu>
2023-10-20 12:28:12 -0400

    fix iteration error in test

--- tests/guess_num_test.py
+++ tests/guess_num_test.py
@@ -2,7 +2,7 @@ import random
 import re
 import guess_the_number
 
-inputs = iter(random.sample(range(1, 101), 15))
+inputs = iter(random.sample(range(1, 101), 50))
 
 def test_check_guess(capsys, monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
